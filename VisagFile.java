bdf5c1e833679a7d997cb1301907c189b0e6b9ab
anitha.nithi <meetanitha.n@gmail.com>
2022-08-06 11:23:21 +0530

    initialize git

---
+++ VisagFile.java
@@ -0,0 +1,13 @@
+
+	   
+	   
+	   
+		class AnithaFile
+		{
+		Public static void main ( string []args){
+			System.out.println("output");
+		}
+		
+		
+		}
+		
---
+++ customer.java
@@ -0,0 +1,34 @@
+class Customer
+
+{
+
+int staff_id ;
+String first_name ;
+String last_name ;
+String email;
+String phone ;
+int active ;
+int store_id;
+int manager_id;
+
+
+}
+
+class OrderItems
+
+{
+
+int order_id ;
+int customer_id;
+int order_status ;
+int order_date;
+String store_name ;
+}
+
+class orders 
+
+{
+int order_id ;
+int customer_id ;
+String required_date ;
+}
